22449f57af37bffa38bf3b801ede4c14f34015e4
Nicholas Otieno <niklasoti@gmail.com>
2020-10-27 12:16:43 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,58 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+// set env
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: 'Image Shrink',
+    width: 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev
+  })
+
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile(`${__dirname}/app/index.html`);
+
+}
+
+app.on('ready', () => {
+  createMainWindow()
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+
+  mainWindow.on('closed', () => mainWindow = null);
+});
+
+const menu = [
+  ...(isMac ? [{ role: 'appMenu' }] : []),
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        click: () => app.quit()
+      }
+    ]
+  }
+]
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
