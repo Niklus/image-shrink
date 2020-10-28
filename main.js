323ad81c1878f9762c6e3e594eed85c41026e8ff
Nicholas Otieno <niklasoti@gmail.com>
2020-10-28 15:51:11 +0100

    Update

--- main.js
+++ main.js
@@ -1,26 +1,57 @@
-const { app, BrowserWindow, Menu } = require('electron');
+
+const path = require('path');
+const os = require('os');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const slash = require('slash');
+const log = require('electron-log');
+
 
 // set env
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     title: 'Image Shrink',
-    width: 500,
+    width: isDev ? 800 : 500,
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    resizable: isDev
+    resizable: isDev,
+    backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: true
+    }
+
   })
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
   mainWindow.loadFile(`${__dirname}/app/index.html`);
-
 }
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: 'About Image Shrink',
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: 'white'
+  })
+  aboutWindow.loadFile(`${__dirname}/app/about.html`);
+}
+
 
 app.on('ready', () => {
   createMainWindow()
@@ -28,31 +59,86 @@ app.on('ready', () => {
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
 
-
   mainWindow.on('closed', () => mainWindow = null);
 });
 
 const menu = [
-  ...(isMac ? [{ role: 'appMenu' }] : []),
-  {
-    label: 'File',
+  ...(isMac ? [{
+    label: app.name,
     submenu: [
       {
-        label: 'Quit',
-        click: () => app.quit()
+        label: 'About',
+        click: createAboutWindow
       }
     ]
-  }
+  }] : []),
+
+  {
+    role: 'fileMenu'
+  },
+
+  ...(!isMac ? [{
+    label: 'Help',
+    submenu: [
+      {
+        label: 'About',
+        click: createAboutWindow
+      }
+    ]
+
+  }] : []),
+
+  ...(isDev ? [
+    {
+      label: 'Developer',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { type: 'separator' },
+        { role: 'toggledevtools' }
+      ]
+    }
+  ] : [])
 ]
+
+ipcMain.on('image:minimize', (event, data) => {
+  data.dest = path.join(os.homedir(), 'imageshrink');
+  shrinkImage(data);
+});
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality]
+        })
+      ]
+    });
+
+    log.info(files);
+
+    shell.openPath(dest);
+
+    mainWindow.webContents.send('image:done');
+
+  } catch (err) {
+    log.error(err);
+  }
+}
 
 app.on('window-all-closed', () => {
   if (!isMac) {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
-})
+});
